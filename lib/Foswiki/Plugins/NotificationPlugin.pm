f197906507b86513bca431c7bf1b42ab6413fcf2
HarlanStenn <HarlanStenn@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-01-27 01:57:28 +0000

    Item8428: Initial pass at converting NotificationPlugin
    git-svn-id: http://svn.foswiki.org/trunk/NotificationPlugin@6158 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/NotificationPlugin.pm
+++ lib/Foswiki/Plugins/NotificationPlugin.pm
@@ -16,7 +16,7 @@
 #
 # =========================
 #
-# This is an empty TWiki plugin. Use it as a template
+# This is an empty Foswiki plugin. Use it as a template
 # for your own plugins; see %SYSTEMWEB%.Plugins for details.
 #
 # Each plugin is a package that may contain these functions:        VERSION:
@@ -42,15 +42,15 @@
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
-# in the TWiki::Func module. Do not reference any functions or
-# variables elsewhere in TWiki!!
+# NOTE: To interact with Foswiki use the official Foswiki functions 
+# in the Foswiki::Func module. Do not reference any functions or
+# variables elsewhere in Foswiki!!
 
 
 # =========================
-package TWiki::Plugins::NotificationPlugin;    # change the package name and $pluginName!!!
+package Foswiki::Plugins::NotificationPlugin;    # change the package name and $pluginName!!!
 
-use TWiki::Store;
+use Foswiki::Store;
 
 # =========================
 use vars qw(
@@ -58,7 +58,7 @@ use vars qw(
         $sender @users $debug @sections
     );
 
-# This should always be $Rev$ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
 $VERSION = '$Rev$';
@@ -76,8 +76,8 @@ sub initPlugin
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if( $Foswiki::Plugins::VERSION < 1 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
         return 0;
     }
 
@@ -91,11 +91,11 @@ sub initPlugin
       "(Regex) notifications"
     );
     
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" ) || 0;
-    $sender = TWiki::Func::getPreferencesValue( "\U$pluginName\E_SENDER" ) || "TWiki NotificationPlugin";
+    $debug = Foswiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" ) || 0;
+    $sender = Foswiki::Func::getPreferencesValue( "\U$pluginName\E_SENDER" ) || "Foswiki NotificationPlugin";
     
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    Foswiki::Func::writeDebug( "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
     return 1;
 }
 
@@ -104,7 +104,7 @@ sub commonTagsHandler
 {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    Foswiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
 
     # This is the place to define customized tags and variables
     # Called by sub handleCommonTags, after %INCLUDE:"..."%
@@ -118,53 +118,53 @@ sub beforeSaveHandler
 {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-  TWiki::Func::writeDebug( "- ${pluginName}::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
+  Foswiki::Func::writeDebug( "- ${pluginName}::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
 
-  # This handler is called by TWiki::Store::saveTopic just before the save action.
-  # New hook in TWiki::Plugins $VERSION = '1.010'
+  # This handler is called by Foswiki::Store::saveTopic just before the save action.
+  # New hook in Foswiki::Plugins $VERSION = '1.010'
 
-  my $wikiUser = &TWiki::userToWikiName( $user, 1 );
+  my $wikiUser = &Foswiki::userToWikiName( $user, 1 );
   my @notifyUsers = ();
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 0 ) );
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 1 ) );
   push( @notifyUsers, getUsersToNotify( $_[2], $_[1], 2 ) );
-  #&TWiki::Func::writeDebug( "COUNT = $#notifyUsers" );
+  #&Foswiki::Func::writeDebug( "COUNT = $#notifyUsers" );
   my $subject = "Topic $_[2].$_[1] has been changed by $wikiUser.";
-  my $body = "Topic ".&TWiki::Func::getScriptUrl( $_[2], $_[1], "view" )." has been changed by $wikiUser at " . &TWiki::Func::formatTime( time() ) . " GMT";
+  my $body = "Topic ".&Foswiki::Func::getScriptUrl( $_[2], $_[1], "view" )." has been changed by $wikiUser at " . &Foswiki::Func::formatTime( time() ) . " GMT";
   notifyUsers( \@notifyUsers, $subject, $body );
 }
 
 # =========================
 sub getUsers {
   my @result;
-  #&TWiki::Func::writeDebug( &TWiki::Func::getDataDir()."/".&TWiki::Func::getMainWebname() );
-  if ( opendir( DIR, &TWiki::Func::getDataDir()."/".&TWiki::Func::getMainWebname() ) ) {
+  #&Foswiki::Func::writeDebug( &Foswiki::Func::getDataDir()."/".&Foswiki::Func::getMainWebname() );
+  if ( opendir( DIR, &Foswiki::Func::getDataDir()."/".&Foswiki::Func::getMainWebname() ) ) {
     my @topics = grep( /NotifyList.*txt$/, readdir( DIR ) );
     foreach my $name ( @topics ) {
       $name =~ /^(.*?)NotifyList/;
-      #&TWiki::Func::writeDebug( "NAME = $1" );
+      #&Foswiki::Func::writeDebug( "NAME = $1" );
       $result[++$#result] = $1 if ( $1 ne "" ); 
     }
     closedir( DIR );
   }
-  #&TWiki::Func::writeDebug( "USERS = $#result" );
+  #&Foswiki::Func::writeDebug( "USERS = $#result" );
   return @result;
 }
 
 sub getUsersToNotify {
   my ( $tweb, $ttopic, $section ) = @_;
   my @result;
-  #&TWiki::Func::writeDebug( "TYPE = $type" );
+  #&Foswiki::Func::writeDebug( "TYPE = $type" );
   foreach my $tmp ( @users ) {
-    #&TWiki::Func::writeDebug( "TMP = $tmp" );
-    my $text = &TWiki::Func::readTopic( &TWiki::Func::getMainWebname(), "$tmp"."NotifyList" );
+    #&Foswiki::Func::writeDebug( "TMP = $tmp" );
+    my $text = &Foswiki::Func::readTopic( &Foswiki::Func::getMainWebname(), "$tmp"."NotifyList" );
     my $test = "";
     foreach my $line ( split( /\n/, $text ) ) {
       $line =~ s/\s+$//;
-      #&TWiki::Func::writeDebug( "LINE = $line" );
-      #&TWiki::Func::writeDebug( "TEST = $test" );
+      #&Foswiki::Func::writeDebug( "LINE = $line" );
+      #&Foswiki::Func::writeDebug( "TEST = $test" );
       $test = "" if ( ( $test ne "" ) && ( $line !~ /^\s*\*/ ) );
-      #&TWiki::Func::writeDebug( "TEST = $test" );
+      #&Foswiki::Func::writeDebug( "TEST = $test" );
       if ( $test eq "Topic" ) {
         $line =~ /\s*\*\s(.*?)\.(.*)/;
         if ( ( $tweb eq $1 ) && ( $ttopic eq $2 ) ) {
@@ -195,24 +195,24 @@ sub getNotificationsOfUser {
   my $section = shift;
   my $text = shift || "";
   my $meta;
-  #&TWiki::Func::writeDebug( "NTF:getNotificationsOfUser: WHO = $who, SCT = $section, TXT = ".length( $text ) );
+  #&Foswiki::Func::writeDebug( "NTF:getNotificationsOfUser: WHO = $who, SCT = $section, TXT = ".length( $text ) );
   ( $meta, $text ) = checkUserNotifyList( $who ) if ( $text eq "" );
   my @result;
-  #&TWiki::Func::writeDebug( "USER = $tuser" );
+  #&Foswiki::Func::writeDebug( "USER = $tuser" );
   $test = "";
   foreach my $line ( split( /\n/, $text ) ) {
-      #&TWiki::Func::writeDebug( "LINE = $line" );
+      #&Foswiki::Func::writeDebug( "LINE = $line" );
     while ( ( $line =~ /\n$/ ) || ( $line =~ /\r$/ ) ) {
       chop( $line );
     }
     last if ( ( $test ne "" ) && ( $line !~ /^\s*\*/ ) );
     if ( $test eq "Topic" ) {
         $line =~ /\s*\*\s(.*?)\.(.*)/;
-        #&TWiki::Func::writeDebug( "TOPIC = $1.$2" );
+        #&Foswiki::Func::writeDebug( "TOPIC = $1.$2" );
         $result[++$#result] = "$1.$2";
     } elsif ( ( $test eq "Web" ) || ( $test eq "Regex" ) ) {
       $line =~ /\s*\*\s(.*)/;
-      #&TWiki::Func::writeDebug( "RESULT = $1" );
+      #&Foswiki::Func::writeDebug( "RESULT = $1" );
       $result[++$#result] = $1;
       }
     $test = $1 if ( $line =~ /$sections[$section]/ );
@@ -222,28 +222,28 @@ sub getNotificationsOfUser {
 
 sub notifyUsers {
   my ( $notifyUsers, $subject, $body ) = @_;
-  #&TWiki::Func::writeDebug( "NT = $notifyUsers" );
+  #&Foswiki::Func::writeDebug( "NT = $notifyUsers" );
   foreach my $tmp ( @{$notifyUsers} ) {
-    &TWiki::Func::writeDebug( "MAIL SENT TO $tmp ..." );
-    #my $email = "Date: ".&TWiki::handleTime("","gmtime")."\n";
+    &Foswiki::Func::writeDebug( "MAIL SENT TO $tmp ..." );
+    #my $email = "Date: ".&Foswiki::handleTime("","gmtime")."\n";
     my $email .= "From: $sender\n";
     $email .= "To: ".getUserEmail( $tmp )."\n";
     $email .= "CC: \n";
     $email .= "Subject: $subject\n\n";
     $email .= "$body\n";
-    #&TWiki::Func::writeDebug( "Sending mail to $tmp ..." );
-    my $error = &TWiki::Net::sendEmail( $email );
+    #&Foswiki::Func::writeDebug( "Sending mail to $tmp ..." );
+    my $error = &Foswiki::Net::sendEmail( $email );
     if ( $error ) {
-      &TWiki::Func::writeDebug( "ERROR WHILE SENDING MAIL - $error" );
+      &Foswiki::Func::writeDebug( "ERROR WHILE SENDING MAIL - $error" );
     }
   }  
 }
 
 sub getUserEmail {
   my $who = shift;
-  my @emails = &TWiki::getEmailOfUser( $who );
+  my @emails = &Foswiki::getEmailOfUser( $who );
   return "" if ( $#emails < 0 );
-  #&TWiki::Func::writeDebug( "USER: $user, EMAIL $emails[0]" );
+  #&Foswiki::Func::writeDebug( "USER: $user, EMAIL $emails[0]" );
   return $emails[0];
 }
 
@@ -253,14 +253,14 @@ sub addItemToNotifyList {
   my $section = shift;
   my $meta = shift || "";
   my $text = shift || "";
-  #&TWiki::Func::writeDebug( "NTF:addItemToNotifyList: adding '$what' to section $sections[$section]" );
+  #&Foswiki::Func::writeDebug( "NTF:addItemToNotifyList: adding '$what' to section $sections[$section]" );
   ( $meta, $text ) = checkUserNotifyList( $who ) if ( $text eq "" );
   return ( $meta, $text ) if ( isItemInSection( $who, $what, $section, $text ) );
-  my @items = &TWiki::Plugins::NotificationPlugin::getNotificationsOfUser( $TWiki::wikiName, $section, $text );
+  my @items = &Foswiki::Plugins::NotificationPlugin::getNotificationsOfUser( $Foswiki::wikiName, $section, $text );
   my $newText = "";
   my $tmp = 0;
   foreach $line ( split( /\n/, $text ) ) {
-    #&TWiki::Func::writeDebug( "LINE = $line" );
+    #&Foswiki::Func::writeDebug( "LINE = $line" );
     $tmp = 0 if ( $line =~ /^---\+\+\s/ && $tmp );
     $tmp = 1 if ( $line =~ /$sections[$section]/ );
     if ( $tmp == 0 ) {
@@ -285,10 +285,10 @@ sub removeItemFromNotifyList {
   my $section = shift;
   my $meta = shift || "";
   my $text = shift || "";
-  #&TWiki::Func::writeDebug( "NTF:removeItemFromNotifyList: removing '$what' from section $sections[$section]" );
+  #&Foswiki::Func::writeDebug( "NTF:removeItemFromNotifyList: removing '$what' from section $sections[$section]" );
   ( $meta, $text ) = checkUserNotifyList( $who ) if ( $text eq "" );
   return ( $meta, $text ) if ( !isItemInSection( $who, $what, $section, $text ) );
-  my @items = &TWiki::Plugins::NotificationPlugin::getNotificationsOfUser( $TWiki::wikiName, $section, $text );
+  my @items = &Foswiki::Plugins::NotificationPlugin::getNotificationsOfUser( $Foswiki::wikiName, $section, $text );
   my $newText = "";
   my $tmp = 0;
   foreach $line ( split( /\n/, $text ) ) {
@@ -300,7 +300,7 @@ sub removeItemFromNotifyList {
     if ( $tmp == 1 ) {
       $newText .= "$line\n";
       foreach my $item ( @items ) {
-        #&TWiki::Func::writeDebug( "ITEM = ^$item^" );
+        #&Foswiki::Func::writeDebug( "ITEM = ^$item^" );
         $newText .= "   * $item\n" if ( $item ne $what );
       }
       $tmp = 2;
@@ -314,28 +314,28 @@ sub checkUserNotifyList {
   my $who = shift;
   my $tmpText;
   my $tmpMeta;
-  #&TWiki::Func::writeDebug( "NTF:checkUserNotifyList: WHO = $who" );
-  if ( !&TWiki::Func::topicExists( "Main", $who."NotifyList" ) ) {
-    &TWiki::Func::writeDebug( "TEST1" );
-    ( $tmpMeta, $tmpText ) = &TWiki::Func::readTopic( "Main", "NotificationPluginListTemplate" );
+  #&Foswiki::Func::writeDebug( "NTF:checkUserNotifyList: WHO = $who" );
+  if ( !&Foswiki::Func::topicExists( "Main", $who."NotifyList" ) ) {
+    &Foswiki::Func::writeDebug( "TEST1" );
+    ( $tmpMeta, $tmpText ) = &Foswiki::Func::readTopic( "Main", "NotificationPluginListTemplate" );
     $tmpMeta->put( "TOPICPARENT", ( "name" => $who ) );
     saveUserNotifyList( $who, $tmpMeta, $tmpText );
   } else {
-    ( $tmpMeta, $tmpText ) = &TWiki::Func::readTopic( "Main", $who."NotifyList" );
+    ( $tmpMeta, $tmpText ) = &Foswiki::Func::readTopic( "Main", $who."NotifyList" );
   }
   return ( $tmpMeta, $tmpText );
 }
 
 sub saveUserNotifyList {
   my ( $who, $meta, $text ) = @_;
-  #&TWiki::Func::writeDebug( "NTF:saveUserNotifyList: Saving Main.".$who."NotifyList topic..." );
+  #&Foswiki::Func::writeDebug( "NTF:saveUserNotifyList: Saving Main.".$who."NotifyList topic..." );
   $text =~ s/   /\t/g;
   my $repRev = "repRev";
-  $repRev = "" if ( !&TWiki::Func::topicExists( "Main", $who."NotifyList" ) );
-  my $error = &TWiki::Store::saveTopic( "Main", $who."NotifyList", $text, $meta, $repRev, "checked", "checked" );
+  $repRev = "" if ( !&Foswiki::Func::topicExists( "Main", $who."NotifyList" ) );
+  my $error = &Foswiki::Store::saveTopic( "Main", $who."NotifyList", $text, $meta, $repRev, "checked", "checked" );
   if ( $error ) {
-    my $url = &TWiki::Func::getOopsUrl( $web, $topic, "oopssaveerr", $error );
-    &TWiki::Func::redirectCgiQuery( $query, $url );
+    my $url = &Foswiki::Func::getOopsUrl( $web, $topic, "oopssaveerr", $error );
+    &Foswiki::Func::redirectCgiQuery( $query, $url );
   }    
 }
 
@@ -344,7 +344,7 @@ sub isItemInSection {
   my $what = shift;
   my $section = shift;
   my $text = shift || "";
-  #&TWiki::Func::writeDebug( "NTF:isItemInSection: WHO = $who, WHT = $what, SCT = $section, TXT = ".length( $text ) );
+  #&Foswiki::Func::writeDebug( "NTF:isItemInSection: WHO = $who, WHT = $what, SCT = $section, TXT = ".length( $text ) );
   my $meta;
   ( $meta, $text ) = checkUserNotifyList( $who ) if ( $text eq "" );
   my @items = getNotificationsOfUser( $who, $section, $text );
@@ -365,16 +365,16 @@ sub showNotifyButtons {
   $popup = $1 if ( $attrs =~ /popup=\"(.*?)\"/ );
   $opt = $1 if ( $attrs =~ /optional=\"(.*?)\"/ );
   my $text = "";
-  if ( $TWiki::wikiName ne "WikiGuest" ) {
-    $tinOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web.$topic", 0 ) );
-    $winOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web", 1 ) );
-    $tnOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web.$topic", 3 ) );
-    $wnOn = "off" if ( !isItemInSection( $TWiki::wikiName, "$web", 4 ) );
-    $text .= "<input onClick='javascript:window.open(\"".&TWiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?popup=on\");' type='button' value='Popup'>&nbsp;" if ( $popup eq "on" );
-    $text .= "<input onClick='javascript:location.href(\"".&TWiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=TIN&action=$tmp{$tinOn}&$opt\");' type='button' value='TIN $tinOn' title='Topic immediate notifications! Click to set it $tmp{$tinOn}!'>&nbsp;" if ( $tin eq "on" );
-    $text .= "<input onClick='javascript:location.href(\"".&TWiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=WIN&action=$tmp{$winOn}&$opt\");' type='button' value='WIN $winOn' title='Web immediate notifications! Click to set it $tmp{$winOn}!'>&nbsp;" if ( $win eq "on" );
-    $text .= "<input onClick='javascript:location.href(\"".&TWiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=TN&action=$tmp{$tnOn}&$opt\");' type='button' value='TN $tnOn' title='Topic notifications! Click to set it $tmp{$tnOn}!'>&nbsp;" if ( $tn eq "on" );
-    $text .= "<input onClick='javascript:location.href(\"".&TWiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=WN&action=$tmp{$wnOn}&$opt\");' type='button' value='WN $wnOn' title='Web notifications! Click to set it $tmp{$wnOn}!'>&nbsp;" if ( $wn eq "on" );
+  if ( $Foswiki::wikiName ne "WikiGuest" ) {
+    $tinOn = "off" if ( !isItemInSection( $Foswiki::wikiName, "$web.$topic", 0 ) );
+    $winOn = "off" if ( !isItemInSection( $Foswiki::wikiName, "$web", 1 ) );
+    $tnOn = "off" if ( !isItemInSection( $Foswiki::wikiName, "$web.$topic", 3 ) );
+    $wnOn = "off" if ( !isItemInSection( $Foswiki::wikiName, "$web", 4 ) );
+    $text .= "<input onClick='javascript:window.open(\"".&Foswiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?popup=on\");' type='button' value='Popup'>&nbsp;" if ( $popup eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&Foswiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=TIN&action=$tmp{$tinOn}&$opt\");' type='button' value='TIN $tinOn' title='Topic immediate notifications! Click to set it $tmp{$tinOn}!'>&nbsp;" if ( $tin eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&Foswiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=WIN&action=$tmp{$winOn}&$opt\");' type='button' value='WIN $winOn' title='Web immediate notifications! Click to set it $tmp{$winOn}!'>&nbsp;" if ( $win eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&Foswiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=TN&action=$tmp{$tnOn}&$opt\");' type='button' value='TN $tnOn' title='Topic notifications! Click to set it $tmp{$tnOn}!'>&nbsp;" if ( $tn eq "on" );
+    $text .= "<input onClick='javascript:location.href(\"".&Foswiki::Func::getScriptUrl( $web, $topic, "changenotify" )."?what=WN&action=$tmp{$wnOn}&$opt\");' type='button' value='WN $wnOn' title='Web notifications! Click to set it $tmp{$wnOn}!'>&nbsp;" if ( $wn eq "on" );
   }
   return $text;
 }
